fffe3d7d1c99c2962c6ec088244237dd8f57c001
Neto Devel <josevieira.dev@gmail.com>
2016-02-26 15:12:21 +0000

    Add authors and associations

--- sed-blog/db/schema.rb
+++ sed-blog/db/schema.rb
@@ -11,13 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226145038) do
+ActiveRecord::Schema.define(version: 20160226150336) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.text     "content",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "author_id",  limit: 4
   end
 
+  add_index "articles", ["author_id"], name: "index_articles_on_author_id", using: :btree
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_foreign_key "articles", "authors"
 end
