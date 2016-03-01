3a8a25a2b3140834057430d6d4a919d5388a7c30
NetoDevel <josevieira.dev@gmail.com>
2016-03-01 12:55:56 +0000

    gem simple form

--- sed-blog/db/schema.rb
+++ sed-blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226150336) do
+ActiveRecord::Schema.define(version: 20160226201655) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(version: 20160226150336) do
     t.string   "email",      limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "articles", "authors"
