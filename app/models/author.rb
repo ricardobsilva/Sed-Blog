f97d25ad56f4a103462af15292b52c406c676a9e
Ricardo <ricardo.b.silva13@gmail.com>
2016-03-02 14:15:51 -0300

    tests for author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,4 @@
 class Author < ActiveRecord::Base
 	has_many :articles
+	validates :name, :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Author, type: :model do
+  it{ expect(subject).to validate_presence_of(:name) }
+  it{ expect(subject).to validate_presence_of(:email) }
+  it{ expect(subject).to have_many(:articles)}
+
+  it 'valid factory' do
+  	expect(build(:author)).to be_valid
+  end
+
+  context 'is invalid' do
+  	it 'without name' do
+  		author = build(:author , name: nil)
+  		author.valid?
+  		expect(author.errors[:name]).to include("can't be blank")
+  	end
+  	it 'without email'do
+	  	author = build(:author, email: nil)
+	  	author.valid?
+	  	expect(author.errors[:email]).to include("can't be blank")
+  	end
+  end
 end
